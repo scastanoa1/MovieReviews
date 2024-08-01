0245f0de8737490b993ba249da95477780138a74
Sebastian Castaño <scastanoa1@eafit.edu.co>
2024-08-01 11:04:33 -0500

    cambios en la función home

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,7 @@ from django. http import HttpResponse
 def home( request):
     #return HttpResponse(' <h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Sebastián Castaño'})
 
 def About(request):
     return HttpResponse(' <h1>Welcom to About Page</h1>')
