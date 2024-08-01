59109286671fcb94538ad8c1e0a191199d493787
Sebastian Castaño <scastanoa1@eafit.edu.co>
2024-08-01 11:58:59 -0500

    Cambios en la clase views.py

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django. http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 def home( request):
     #return HttpResponse(' <h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Sebastián Castaño'})
+    #return render(request, 'home.html', {'name':'Sebastián Castaño'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.object.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 
 def About(request):
     #return HttpResponse(' <h1>Welcom to About Page</h1>')
